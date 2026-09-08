e0fb37aab8b9c49408526358a8eaeb0078fb5650
_DanL@n_ <leonardo.landeo@utec.edu.pe>
2026-09-08 15:37:46 -0500

    Postgre configurated and errors in service fixed (#2)

--- src/main/java/com/example/studystreak/service/TrackingListService.java
+++ src/main/java/com/example/studystreak/service/TrackingListService.java
@@ -1,12 +1,13 @@
 package com.example.studystreak.service;
 
+import com.example.studystreak.repository.TrackingLinkRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TrackingLinkRepository {
+public class TrackingListService {
     private final TrackingLinkRepository trackingLinkRepository;
 
-    public TrackingLinkRepository(TrackingLinkRepository trackingLinkRepository) {
+    public TrackingListService(TrackingLinkRepository trackingLinkRepository) {
         this.trackingLinkRepository = trackingLinkRepository;
     }
 }
