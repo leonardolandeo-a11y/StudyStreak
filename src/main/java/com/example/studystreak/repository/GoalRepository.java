96df3e63a29682594c8ad55f49bdc4a041725f9f
_DanL@n_ <leonardo.landeo@utec.edu.pe>
2026-09-08 19:20:43 -0500

    UserService, GoalService, and DailyRecordService were implemented (#8)

--- src/main/java/com/example/studystreak/repository/GoalRepository.java
+++ src/main/java/com/example/studystreak/repository/GoalRepository.java
@@ -3,5 +3,8 @@ package com.example.studystreak.repository;
 import com.example.studystreak.model.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GoalRepository extends JpaRepository<Goal,Long> {
+    public List<Goal> findByUserId(Long UserId);
 }
--- src/main/java/com/example/studystreak/service/UserService.java
+++ src/main/java/com/example/studystreak/service/UserService.java
@@ -1,8 +1,14 @@
 package com.example.studystreak.service;
 
+import com.example.studystreak.dto.User.UserRequestDTO;
+import com.example.studystreak.dto.User.UserResponseDTO;
+import com.example.studystreak.model.User;
 import com.example.studystreak.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
 
 @Service
 public class UserService {
@@ -13,5 +19,31 @@ public class UserService {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
     }
+    public UserResponseDTO createUser(UserRequestDTO userRequestDTO){
+        User user = modelMapper.map(userRequestDTO, User.class);
+        user.setRegistrationDate(LocalDate.now());
+        user.setActive(true);
 
+        user = userRepository.save(user);
+        return modelMapper.map(user, UserResponseDTO.class);
+    }
+    public UserResponseDTO getUserById(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(); // Exception (No implemented yet)
+        return modelMapper.map(user, UserResponseDTO.class);
+    }
+
+    public UserResponseDTO updateUser(Long userId, UserRequestDTO userRequestDTO){
+        User user = userRepository.findById(userId).orElseThrow(); // Exception (No implemented yet)
+        user.setEmail(userRequestDTO.getEmail());
+        user.setPassword(userRequestDTO.getPassword());
+        user.setUsername(userRequestDTO.getUsername());
+        user.setTimeZone(userRequestDTO.getTimeZone());
+
+        userRepository.save(user);
+        return modelMapper.map(user, UserResponseDTO.class);
+    }
+    public void deleteUser(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(); // Exception (No implemented yet)
+        userRepository.delete(user);
+    }
 }
