300acd50a9ec52a373ec52763f53734979b80fa0
Leonard <leonardo.landeo@utec.edu.pe>
2026-09-25 17:34:32 -0500

    Endpoint moved. Register endopoint added

--- src/main/java/com/example/studystreak/controller/AuthController.java
+++ src/main/java/com/example/studystreak/controller/AuthController.java
@@ -2,7 +2,12 @@ package com.example.studystreak.controller;
 
 import com.example.studystreak.dto.Auth.LoginRequestDTO;
 import com.example.studystreak.dto.Auth.LoginResponseDTO;
+import com.example.studystreak.dto.User.UserRequestDTO;
+import com.example.studystreak.dto.User.UserResponseDTO;
 import com.example.studystreak.service.AuthService;
+import com.example.studystreak.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
-
-    public AuthController(AuthService authService){
+    private final UserService userService;
+    public AuthController(AuthService authService,UserService userService){
         this.authService = authService;
+        this.userService = userService;
+    }
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@Valid @RequestBody UserRequestDTO request) {
+
+        UserResponseDTO createdUser = userService.createUser(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     @PostMapping("/login")
--- src/main/java/com/example/studystreak/security/SecurityConfig.java
+++ src/main/java/com/example/studystreak/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.studystreak.security;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,16 +10,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -70,9 +61,11 @@ public class SecurityConfig {
                                 })
                 )
 
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/login").permitAll().requestMatchers(HttpMethod.POST, "/users")
-                                .permitAll().anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/login", "/register")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
                 .addFilterBefore(
                         jwtRequestFilter,
